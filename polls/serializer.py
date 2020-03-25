98b23e4a9d536f509f0adb89eecf3528d4f69b68
Carlos Hernandez <soycarlosluis862@gmail.com>
2020-03-25 19:26:57 -0400

    api

---
+++ polls/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Question,Choice
+
+class QuestionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Question
+		fields = '__all__'
+class ChoiceSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Choice
+		fields = '__all__'
--- polls/urls.py
+++ polls/urls.py
@@ -10,13 +10,18 @@ urlpatterns = [
 ]
 """
 from django.urls import path
+from rest_framework import routers
+# from . import views
+from .viewsets import QuestionViewSet
 
-from . import views
+router = routers.SimpleRouter()
+router.register(r'question', QuestionViewSet)
 
-app_name = 'polls'
-urlpatterns = [
+#app_name = 'polls'
+urlpatterns = router.urls
+"""[
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-]
+]"""
