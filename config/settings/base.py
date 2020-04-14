82af4ec36b21240965312d4819b1c3e471938110
Carlos Hernandez <soycarlosluis862@gmail.com>
2020-04-14 14:01:55 -0400

    finish

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,8 +1,6 @@
-
-
 import os
 from datetime import timedelta
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,17 +22,20 @@ ALLOWED_HOSTS = []
 
 
 # DATABASES = dj_database_url.config(default='sqlite:///sqllite')
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+#updated
+DATABASES = {'default': dj_database_url.config(default='sqlite:///C:/Users/Carlos/Documents/django/encuesta/db.sqlite3')}
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'apps.polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('', include('apps.polls.urls')),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
